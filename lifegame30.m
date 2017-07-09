f4444337d36721979bd61248b569835e42e1af2a
bwxzgeretou <bwxzgeretou@gmail.com>
2017-07-09 15:37:36 +0900

    fix: used 'same' option with conv2()

--- lifegame30.m
+++ lifegame30.m
@@ -8,13 +8,10 @@ function [] = lifegame30()
     while isempty(find(lifegame~=0, 1))
         lifegame = edge(rgb2gray(snapshot(camera)), 'Canny');
     end
-    [row, col] = size(lifegame);
 
     while 1
         imshow(lifegame,'Border','tight');
-        
-        alive_count = conv2(lifegame, kernel);
-        alive_count = alive_count(2:row+1, 2:col+1);
+        alive_count = conv2(lifegame, kernel, 'same');
         lifegame = (alive_count == 3) | (lifegame & (alive_count == 2));
     end
 end
