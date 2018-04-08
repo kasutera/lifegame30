c5d017b903cc26569c0f6b1cc94d4e46851b1be9
bwxzgeretou <bwxzgeretou@gmail.com>
2018-04-08 16:57:13 +0900

    add button for matlab version

--- lifegame30.m
+++ lifegame30.m
@@ -1,17 +1,20 @@
 function [] = lifegame30()
-    camera = webcam();
-    kernel = [1, 1, 1;
-              1, 0, 1;
-              1, 1, 1];
-    lifegame = [];
-    
-    while isempty(find(lifegame~=0, 1))
-        lifegame = edge(rgb2gray(snapshot(camera)), 'Canny');
-    end
+camera = webcam();
+kernel = [1, 1, 1;
+          1, 0, 1;
+          1, 1, 1];
+lifegame = [];
+uicontrol('Style', 'pushbutton', 'String', 'Start', 'Callback', @newgame);
+ax = axes;
 
-    while 1
-        imshow(lifegame,'Border','tight');
-        alive_count = conv2(lifegame, kernel, 'same');
-        lifegame = (alive_count == 3) | (lifegame & (alive_count == 2));
+while 1
+    imshow(lifegame, 'parent', ax);
+    drawnow();
+    alive_count = conv2(double(lifegame), kernel, 'same');
+    lifegame = (alive_count == 3) | (lifegame & (alive_count == 2));
+end
+
+    function newgame(~, ~)
+        lifegame = edge(rgb2gray(snapshot(camera)), 'Canny');
     end
 end
